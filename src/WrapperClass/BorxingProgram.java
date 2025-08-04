f4eeb20fb0ac3c0bdf059be867fdae778f0536c1
sahasra2420030068 <2420030068@klh.edu.in>
2025-08-04 11:35:22 +0530

    Add files via upload

---
+++ src/WrapperClass/BorxingProgram.java
@@ -0,0 +1,14 @@
+package WrapperClass;
+
+public class BorxingProgram {
+
+	
+	public static void main(String[] args) {
+		
+		int x = 200;
+		int y = x;
+		System.out.println(x);
+
+	}
+
+}
---
+++ src/WrapperClass/Boxing.java
@@ -0,0 +1,15 @@
+package WrapperClass;
+
+public class Boxing {
+
+
+	public static void main(String[] args) {
+		
+		int y = 100;
+		Integer i = new Integer(y);
+		System.out.println(i);
+
+	}
+
+}
+
